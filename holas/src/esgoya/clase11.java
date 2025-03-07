0e72c2fcd60d0c4b71a63e77a6440a21032944ea
Fernando80976 <sdnbdx123@gmail.com>
2025-03-07 13:06:48 +0100

    Add files via upload

---
+++ holas/src/esgoya/clase11.java
@@ -0,0 +1,15 @@
+package esgoya;
+
+public class clase11 {
+private String metodo="",metodo2="";
+	
+public clase11(){
+	
+}
+public String metodo1() {
+	return metodo;
+}
+public String metodo2() {
+	return metodo2;
+}
+}
