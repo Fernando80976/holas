8daa79b2ebb5eb58774f08749622020200148b89
Fernando80976 <sdnbdx123@gmail.com>
2025-03-07 13:10:34 +0100

    Add files via upload

---
+++ holas/src/esgoya/clase21.java
@@ -0,0 +1,16 @@
+package esgoya;
+
+public class clase21 {
+	
+		private String metodo="",metodo2="";
+			
+		public clase21(){
+			
+		}
+		public String metodo1() {
+			return metodo;
+		}
+		public String metodo2() {
+			return metodo2;
+		
+}}
